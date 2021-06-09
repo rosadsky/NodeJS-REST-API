b3042855ada909159505be5398ea1056dcfd575d
rosadsky <rosadsky@hotmail.com>
2021-06-09 17:39:13 +0200

    get products by id

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const http = require('http');
-const products = require('./products')
+const { getProducts,getProduct } = require('./controllers/productController');
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/api/products'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(products));
-     
+    if(req.url === '/api/products' && req.method === 'GET'){
+    
+     getProducts(req, res);
+    } else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET'){
+        const id = req.url.split('/')[3];
+        getProduct(req,res,id);
     } else {
         res.writeHead(404, {'Content-Type': 'application/json'});
         res.end(JSON.stringify( {message: 'EROR 404 nothing here'}));
